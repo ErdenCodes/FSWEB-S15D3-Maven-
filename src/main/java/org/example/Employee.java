0c88a58fbd83f1efaf6680eb7ef78dc80947a3d9
ErdenCodes <erden.codes@gmail.com>
2024-12-02 21:42:26 +0300

    S15D3 TAMAM
    Görev tamamlandı.

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public Employee(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id &&
+                Objects.equals(firstname, employee.firstname) &&
+                Objects.equals(lastname, employee.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstname, lastname);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstname + '\'' +
+                ", lastName='" + lastname + '\'' +
+                '}';
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,91 @@
 package org.example;
 
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+        // Employee işlemleri
+        List<Employee> employees = new LinkedList<>();
+        employees.add(new Employee(1, "izzet", "erden"));
+       employees.add(new Employee(1, "izzet", "erden"));
+        employees.add(new Employee(2, "derya", "erden"));
+        employees.add(new Employee(3, "hasan", "demir"));
+        employees.add(new Employee(3, "hasan", "demir"));
+        employees.add(new Employee(4, "ali", "celik"));
+
+        List<Employee> dublicates = findDuplicates(employees);
+        System.out.println(dublicates);
+
+        Map<Integer,Employee> uniques = findUniques(employees);
+        System.out.println(uniques);
+
+
+        System.out.println("*****************");
+        System.out.println(uniques.values());
+        System.out.println("******************");
+        System.out.println(removeDuplicates(employees));
+
+         
+
+        System.out.println(WordCounter.calculatedWord());
+        }
+
+
+
+
+public  static List<Employee> findDuplicates (List<Employee> employees){
+        Map<Integer,Employee> employeeMap = new HashMap<>();
+        List<Employee> dublicates = new LinkedList<>();
+
+        Iterator<Employee> it = employees.iterator();
+
+        while (it.hasNext()) {
+            Employee employeew = it.next();
+             if (employeew == null) {
+                 System.out.println("Null employee founded");
+                 continue;
+             }
+             if(employeeMap.containsKey(employeew.getId())){
+                 dublicates.add(employeew);
+             }else{
+                 employeeMap.put(employeew.getId(),employeew);
+             }
+        }
+        return dublicates;
 }
+
+
+
+public  static Map<Integer , Employee> findUniques(List<Employee> employees){
+    Map<Integer,Employee> employeeMap = new HashMap<>();
+
+
+    Iterator<Employee> it = employees.iterator();
+
+    while (it.hasNext()) {
+        Employee employee = it.next();
+        if (employee == null) {
+            System.out.println("Null employee founded");
+            continue;
+        }
+        if(!employeeMap.containsKey(employee.getId())){
+            employeeMap.put(employee.getId(),employee);
+        }
+        }
+
+    return employeeMap;
+}
+
+public static List<Employee> removeDuplicates (List<Employee> employees){
+        List<Employee> dublicates = findDuplicates(employees);
+        Map<Integer,Employee> uniques = findUniques(employees);
+        List<Employee> duclicatesRemoveList = new LinkedList<>(uniques.values());
+
+        duclicatesRemoveList.removeAll(dublicates);
+        return duclicatesRemoveList;
+}
+
+}
+
+
+
